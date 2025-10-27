08475b0c9920a53dea606130cbdc3da7971e37a7
nathakusuma <nathakusuma07@gmail.com>
2025-10-28 00:58:34 +0700

    style: update color palette and typography styles

--- app/src/main/java/com/nathakusuma/neriva/ui/theme/Theme.kt
+++ app/src/main/java/com/nathakusuma/neriva/ui/theme/Theme.kt
@@ -3,21 +3,20 @@ package com.nathakusuma.neriva.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Primary,
     onPrimary = Color.White,
+    secondary = Secondary,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = InfoColor,
+    background = BackgroundLight,
+    surface = BackgroundSecondary,
+    onBackground = TextPrimary,
+    onSurface = TextPrimary,
+    error = ErrorColor,
+    onError = Color.White
 )
 
 @Composable
